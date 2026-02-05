d41ed26f9ab39eba72d3e90151df230607a1a698
iamdhruv17 <dsinghal7834@gmail.com>
2026-02-05 14:16:21 +0530

    5feb

---
+++ desc/desc.js
@@ -0,0 +1,27 @@
+// const students = {
+//     name:"Dhruv",
+//     age:21,
+//     address:{
+//         city:"gzb",
+//         state:"UP"
+//     }
+// }
+
+// // const name =students.name;
+// // const age=students.age;
+// const {name,age,address:{city}} = students;
+// console.log(name,age,city);
+
+// function displaystu(object){
+//     console.log(`My name is ${object.name} and age is ${object.age}`);
+// }
+// displaystu(students);
+
+// let numbers=[1,2,3];
+// let [a=10,b=20,c=] = numbers;
+// console.log(a,b,c);
+
+let p=10;
+let q=88;
+p,q = q,p;
+console.log(p,q);
